6dc43ce08f3eeff0a89d202da404b21b8060dc77
guanzhen <guanzhen@gmail.com>
2018-06-21 09:35:17 +0800

    Disable debug tab on startup

--- en/onload.js
+++ en/onload.js
@@ -30,7 +30,7 @@ function load_tabbar()
     tabbar.setCustomStyle( 'main_tab' + i, 'gray', 'black', 'font-size:10pt;font-family:Arial;font-weight: bold;' );
   }
 
-  //tabbar.hideTab("main_tab5");
+  tabbar.hideTab("main_tab5");
   tabbar.setTabActive("main_tab1");
 };
 
